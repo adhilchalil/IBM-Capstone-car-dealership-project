973e4bf6b9b079be2ef53d1c42e5ddf31e5dc2fc
adhilchalil <chaliladhil@gmail.com>
2025-03-30 22:25:55 +0530

    linting fixes nodejs and python

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,11 +12,13 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 # Add code for get requests to back end
+
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -31,6 +33,8 @@ def get_request(endpoint, **kwargs):
 
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"/analyze/"+text
     try:
@@ -42,10 +46,12 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 # Add code for posting review
+
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
